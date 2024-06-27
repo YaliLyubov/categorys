d1216f1e1736cf70a5d2fc6e7cdc746214757930
Yali Lyubov <yali.lyuba@mail.ru>
2024-06-27 09:28:30 +0300

    Add changes in style1.css, main.js

--- js/main1.js
+++ js/main1.js
@@ -8,26 +8,15 @@ const rightPos = leaf2.offsetLeft;
 for (let card of cards) {
     card.addEventListener('mouseover', function () {
         card.style.backgroundColor = 'rgba(121, 200, 0, 0.4)';
-        leaf1.style.animation = "steps 500ms ease";
+        leaf1.style.transform = 'translateX(10px)';
+        leaf2.style.transform = 'translateX(10px)';
     });
        
    
     card.addEventListener('mouseout', function () {
         card.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-        leaf2.style.left = rightPos + 10 + 'px';
+        leaf1.style.transform = 'translateX(0)';
+        leaf2.style.transform = 'translateX(0)';
     });
     
 }
-// cards.forEach((card) => {
-//     card.addEventListener('mouseover', function () {
-//         card.style.backgroundColor = 'rgba(121, 200, 0, 0.4)';
-//         // leaf1.style.left = leftPos + 10 + 'px';
-//         leaf1.classList.add('move');
-//     });
-       
-   
-//     card.addEventListener('mouseout', function () {
-//         card.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-//         leaf2.style.left = rightPos + 10 + 'px';
-//     });
-// })
